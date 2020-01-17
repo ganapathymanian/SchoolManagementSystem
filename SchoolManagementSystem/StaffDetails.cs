25d95175911a577bffafc7403385cd1dff2cc634
Ganapathy Subramanian <gasubra@microsoft.com>
2020-01-17 00:32:40 +0000

    Add project files.

---
+++ SchoolManagementSystem/StaffDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagementSystem
+{
+    class StaffDetails
+    {
+        public string StaffName { get; set; }
+        public DateTime DOB { get; set; }
+        public int MobileNumber { get; set; }
+        public string Address { get; set; }
+        public int StaffID { get; set; }
+    }
+}
